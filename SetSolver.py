a85d16bcf56645e010311a584c3aa8939101fc44
Thomas Jansen <thomas.jansen5@ru.nl>
2024-05-03 22:59:50 +0200

    Simple button-label interaction set up in expandable class.

--- SetSolver.py
+++ SetSolver.py
@@ -1,22 +1,40 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import *
 import sys
-class Window(QWidget):
-    def __init__(self):
-        QWidget.__init__(self)
-        layout = QGridLayout()
-        self.setLayout(layout)
-        label = QLabel("Label (0, 0)")
-        layout.addWidget(label, 0, 0)
-        label = QLabel("Label (0, 1)")
-        layout.addWidget(label, 0, 1)
-        label = QLabel("Label (1, 0) spanning 2 columns")
-        layout.addWidget(label, 1, 0, 1, 2)
-        label = QLabel("Label (1, 0) spanning 2 rows")
-        layout.addWidget(label, 0, 2, 2, 1)
 
-app = QApplication(sys.argv)
+WINHEIGHT = 400
+WINWIDTH = 800
 
-screen = Window()
-screen.show()
 
-sys.exit(app.exec_())
+class MyWindow(QMainWindow) :
+    def __init__(self) :
+        super(MyWindow, self).__init__()
+        self.setGeometry(50, 200, WINWIDTH, WINHEIGHT)
+        self.setWindowTitle("Set Solver")
+        self.initUI()
+
+    
+    def initUI(self):
+        
+        self.label = QtWidgets.QLabel("test",self)
+        self.label.setText("test settext")
+        self.label.move(50, 50)
+
+        self.button1 = QPushButton("click me?", self)
+        self.button1.clicked.connect(self.button1Clicked)
+
+    def button1Clicked(self):
+        self.label.setText("test clickedddd button 1?")
+        self.update()
+    
+    def update(self):
+        self.label.adjustSize()
+
+def window():
+    app = QApplication(sys.argv)
+    win = MyWindow()
+    win.show()
+    sys.exit(app.exec_())
+
+window()
