5f17af796ae6248284afd1c5720e564b44911c52
Kim de Waal <kimdewaal8@gmail.com>
2024-05-06 14:45:27 +0200

    Grid Layout added, also added 2 images

--- SetSolver.py
+++ SetSolver.py
@@ -1,40 +1,54 @@
-from PyQt5 import QtWidgets
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import *
+from PyQt6 import QtWidgets
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui import * 
+
 import sys
 
 WINHEIGHT = 400
 WINWIDTH = 800
 
 
-class MyWindow(QMainWindow) :
-    def __init__(self) :
-        super(MyWindow, self).__init__()
+class MyWindow(QWidget) :    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.setGeometry(50, 200, WINWIDTH, WINHEIGHT)
         self.setWindowTitle("Set Solver")
         self.initUI()
 
-    
-    def initUI(self):
-        
-        self.label = QtWidgets.QLabel("test",self)
-        self.label.setText("test settext")
-        self.label.move(50, 50)
 
-        self.button1 = QPushButton("click me?", self)
-        self.button1.clicked.connect(self.button1Clicked)
+    def initUI(self):
+        grid = QGridLayout()
+        self.setLayout(grid)
+        grid.addWidget(QLabel("ello"), 0, 0)
+
+        grid.addWidget(QLabel("nee"), 1, 1)
+
+        image1 = QLabel(self)
+        pixmap = QPixmap("settest01.png")
+        image1.setPixmap(pixmap)
+        grid.addWidget(image1, 0, 1)
+
+        image2 = QLabel(self)
+        pixmap2 = QPixmap("settest01.png")
+        image2.setPixmap(pixmap2)
+        grid.addWidget(image2, 0, 5)
+        
 
     def button1Clicked(self):
-        self.label.setText("test clickedddd button 1?")
+        self.label.setText("hellllll wat")
         self.update()
     
     def update(self):
         self.label.adjustSize()
 
+    
+
 def window():
     app = QApplication(sys.argv)
     win = MyWindow()
     win.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
+
 
 window()
